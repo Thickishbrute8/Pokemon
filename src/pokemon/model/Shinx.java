8c8bf754a7c9ab82f88a5f8e0320ddbdba54ffa4
Britton Perry <britton.perry@hotmail.com>
2016-12-07 09:56:19 -0700

    Shinx class and electric class made

--- src/pokemon/model/Shinx.java
+++ src/pokemon/model/Shinx.java
@@ -1,5 +1,32 @@
 package pokemon.model;
 
-public class Shinx {
-
+public class Shinx extends Pokemon implements Electric 
+{
+	public Shinx()
+	{
+		super("Shinx", 403);
+	}
+	
+	public Shinx(String Name)
+	{
+		Super(name, 403);
+	}
+	
+	/** 
+	 Two parater contructor for Luxio class. If using with Shinx supply 403 for number.
+	 @param name The name of this Shinx
+	 @param number Should be 403 for Shinx.
+	 
+	 
+	 */
+	
+	public Shinx(String name, int number)
+	{
+		super(name, number);
+	}
+	
+	public int WildCharge()
+	{
+		reurn 
+	}
 }
